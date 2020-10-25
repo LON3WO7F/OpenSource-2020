92e8ae190f60386bf06d00fb825263ab460ecf8f
JOY SARKAR <32029022+joy98@users.noreply.github.com>
2020-10-25 18:20:48 +0530

    Code for cleaning text data for ML.

---
+++ Python/cleaning_data_for_ml.py
@@ -0,0 +1,21 @@
+# load data
+filename = 'metamorphosis_clean.txt'
+file = open(filename, 'rt')
+text = file.read()
+file.close()
+# split into words
+from nltk.tokenize import word_tokenize
+tokens = word_tokenize(text)
+# convert to lower case
+tokens = [w.lower() for w in tokens]
+# remove punctuation from each word
+import string
+table = str.maketrans('', '', string.punctuation)
+stripped = [w.translate(table) for w in tokens]
+# remove remaining tokens that are not alphabetic
+words = [word for word in stripped if word.isalpha()]
+# filter out stop words
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+words = [w for w in words if not w in stop_words]
+print(words[:100])
